a9c3c507831a2aaf24d10eaac820d84578339ecb
unstoo <unstoo@gmail.com>
2021-02-12 16:14:23 +0200

    init: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  if (array.length === 0) return 0;
+  if (array.length === 1) return array[0];
+
+  var currentMin = array[0];
+
+  array.forEach(number => {
+    if(number < currentMin) currentMin = number
+  });
+
+  return currentMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  if (array.length === 0) return 0;
+  if (array.length === 1) return array[0];
+
+  var currentMax = array[0];
+
+  array.forEach(number => {
+    if(number > currentMax) currentMax = number
+  });
+
+  return currentMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  if (array.length === 0) return 0;
+  if (array.length === 1) return array[0];
+
+  var denominator = array.length
+  var nominator = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  });
+
+  if (nominator === 0) return 0;
+
+  return nominator / denominator;
 }
